a33eeaad6ddb2ecfd4ed8a5a533779675c62f431
Attesa <calstream.lgn@gmail.com>
2017-09-15 17:19:59 +0300

    dfs

--- 15puzzle/main.cpp
+++ 15puzzle/main.cpp
@@ -7,9 +7,9 @@ const size_t width = 4; //?
 class state
 {
 private:
-	int matrixs[width*width] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
+	int matrix[width*width] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
 	enum direction { Up, Down, Left, Right };
-	int empty_ind;
+	int empty_ind = 15;
 
 	bool is_valid_move(direction d)
 	{
@@ -30,7 +30,29 @@ public:
 	void generate(string s)
 	{
 		assert(s.length() == 16);
-		// switch?
+		int i = 0;
+		switch (s[i])
+		{
+			case 'A':
+				matrix[i++] = 10;
+				break;
+			case 'B':
+				matrix[i++] = 11;
+				break;
+			case 'C':
+				matrix[i++] = 12;
+				break;
+			case 'D':
+				matrix[i++] = 13;
+				break;
+			case 'E':
+				matrix[i++] = 14;
+				break;
+			case 'F':
+				matrix[i++] = 15;
+				break;
+			case 0 ... 9:
+		}
 	}
 
 	void print()
