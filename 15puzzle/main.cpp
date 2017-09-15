151804c3035524a1a8ce18f1fcea023a9339d97d
Attesa <calstream.lgn@gmail.com>
2017-09-15 16:59:42 +0300

    fg

--- 15puzzle/main.cpp
+++ 15puzzle/main.cpp
@@ -1,23 +1,39 @@
 #include<string>
+#include<cassert>
 using namespace std;
 
-const int size = 4;
+const int size = 4; //?
 
 class state
 {
 private:
-	int matrix[4][4];
+	int matrix[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
 	enum direction { Up, Down, Left, Right };
+	int empty_ind;
+
+	bool is_valid_move(direction d)
+	{
+		if (d == Up && empty_ind <= 4)
+			return false;
+		else if (d == Down && empty_ind >= 12)
+			return false;
+		else if (d == Left &&)
+			return false;
+		else if (d == Right &&)
+
+	}
+
 public:
 	state() {}
 	void generate(string s)
 	{
-
+		assert(s.length() == 16);
+		// switch?
 	}
 
 	void print()
 	{
-	
+		
 	}
 
 	bool is_valid()
@@ -27,13 +43,14 @@ public:
 
 	bool is_final()
 	{
-	
+		
 	}
 
 	state make_move(direction d)
 	{
 
 	}
+
 
 };
 
