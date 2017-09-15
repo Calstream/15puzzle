27cb7dd166c201dae61b5f92536e0e52d4e56412
Attesa <calstream.lgn@gmail.com>
2017-09-15 16:40:08 +0300

    d

---
+++ 15puzzle/main.cpp
@@ -0,0 +1,43 @@
+#include<string>
+using namespace std;
+
+const int size = 4;
+
+class state
+{
+private:
+	int matrix[4][4];
+	enum direction { Up, Down, Left, Right };
+public:
+	state() {}
+	void generate(string s)
+	{
+
+	}
+
+	void print()
+	{
+	
+	}
+
+	bool is_valid()
+	{
+
+	}
+
+	bool is_final()
+	{
+	
+	}
+
+	state make_move(direction d)
+	{
+
+	}
+
+};
+
+int main()
+{
+
+}
