ac4f3c32dbd3656a5096d2c4c7bd16df3328c341
Attesa <calstream.lgn@gmail.com>
2017-09-15 17:11:28 +0300

    yt

--- 15puzzle/main.cpp
+++ 15puzzle/main.cpp
@@ -2,24 +2,26 @@
 #include<cassert>
 using namespace std;
 
-const int size = 4; //?
+const size_t width = 4; //?
 
 class state
 {
 private:
-	int matrix[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
+	int matrixs[width*width] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
 	enum direction { Up, Down, Left, Right };
 	int empty_ind;
 
 	bool is_valid_move(direction d)
 	{
-		if (d == Up && empty_ind <= 4)
+		if (d == Up && empty_ind <= width)
 			return false;
-		else if (d == Down && empty_ind >= 12)
+		else if (d == Down && empty_ind >= width*width - width)
 			return false;
-		else if (d == Left &&)
+		else if (d == Left && empty_ind % width == 0)
 			return false;
-		else if (d == Right &&)
+		else if (d == Right && empty_ind % width == width - 1)
+			return false;
+		else return true;
 
 	}
 
